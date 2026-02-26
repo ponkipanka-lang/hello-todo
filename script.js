6c8b2a9e804abdecae009a0a9fcde720cc3002a3
Abdur Rahman Adil <ponkipanka@gmail.com>
2026-02-27 01:32:43 +0600

    Todo

---
+++ script.js
@@ -0,0 +1,27 @@
+const addInput = document.getElementById("add-input");
+const addBtn = document.getElementById("add-btn");
+const result = document.getElementById("result");
+const noTodo = document.getElementById("no-todo");
+let count = 1;
+addInput.placeholder = "Add your 1 item";
+addBtn.addEventListener("click", () => {
+  const inputValue = addInput.value.trim();
+  if (!inputValue) return;
+  noTodo.classList.add("hidden");
+  const resultLiClass = ["bg-warning/20", "rounded", "p-2"];
+  const taskItems = document.createElement("li");
+  taskItems.innerHTML = count + ". " + inputValue;
+  count++;
+
+  taskItems.classList.add(...resultLiClass);
+  result.appendChild(taskItems);
+  addInput.value = "";
+  addInput.placeholder = `Add your ${count} item`;
+});
+
+addInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addBtn.click();
+  }
+});
